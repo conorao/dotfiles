c76c7564a65d8479360539530e1c1271365ef5ba
conorao <conorao@gmail.com>
2020-03-11 12:27:46 -0400

    adapt a small setup script, WIP

--- setup.sh
+++ setup.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+link () {
+  echo "$PROMPT This utility will symlink the files in this repo to the home directory"
+  echo "$PROMPT Proceed? (y/n)"
+  read resp
+  # TODO - regex here?
+  if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+    for file in $( ls -A | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md' ) ; do
+      ln -sv "$PWD/$file" "$HOME"
+    done
+    # TODO: source files here?
+    echo "$PROMPT Symlinking complete"
+  else
+    echo "$PROMPT Symlinking cancelled by user"
+    return 1
+  fi
+}
+
+link
